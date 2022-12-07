6ffbc0b5036e4ce6d83f4a8249ab7145f9779222
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2022-12-06 22:44:16 -0500

    updates
    did some stuff

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log("script file")
+var age=100; //init and assig
+let username="Mike";
+let taxes =11.5;
+const URL = "http://www.hun.com";
+
+console.log(age,username,URL); //displaying the age, username and URL
+
+username="ed"; //changed the username
+taxes=12; //changed the taxes
+console.log(username,URL); //displaying the username and URL
+
+let numberChildren=0;
+let spousename ="Girl";
+let geolocation= "mars";
+let jobtitle= "boss";
+
+//document.write(`<p> You will be a ${jobTitle} in ${geoLeocation}, and married to ${partnersName} with ${numberOfChildren + 1} kids</p>`);
+
+
+document.write(`
+<p>you will be a ${jobtitle}</p> <p>on ${geolocation}</p> <p>and living with ${spousename}</p>`)
+
+
