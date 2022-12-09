a532959a260657a66ad85092ae3d78e140715acc
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2022-12-08 21:38:08 -0500

    again
    takinh a save

---
+++ script2.js
@@ -0,0 +1,30 @@
+// let username = prompt("enter your name here:");
+//let userEmail = prompt("test@test.com:");
+//let password = "123abcdef";
+//let userAge = "21";
+//let couNtry = "usa";
+//let saLary = prompt("enter your salry:");
+//const monTh = 12;
+
+
+//console.log(userEmail, username, saLary)
+
+//document.write(`
+//<p>User: ${username}</p>
+//<p>Email: ${userEmail} </p>
+//<p>salary: ${saLary}</p>
+
+//`) 
+
+//fuction declaration
+function greeting() {
+    console.log("hello");
+    //alert("hello");
+    //document.write("hello");
+    return "Gary"
+}
+function sum() {
+    return 10 + 10;
+}
+
+console.log(sum());
